3faab20082373f565379573b913f6297794c6ff4
Sam Syharath <112458977+samsyharath@users.noreply.github.com>
2024-02-15 16:46:03 -0800

    Created characterData script
    Added Character information: Name, Health, Mana Points, Limit Break Points, and Speed

---
+++ Turn Based RPG/Assets/Scripts/CharacterData.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterData : MonoBehaviour
+{
+    public string characterName;
+
+    public int health;
+    public int currentHealth;
+
+    public int maxManaPoints;
+    public int manaCurrentPoints;
+
+    public float limitBurstPoints;
+    public float currentLBP;
+
+    public float speed;
+    public float currentSpeed;
+
+}
